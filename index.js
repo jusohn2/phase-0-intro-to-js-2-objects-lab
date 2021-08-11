17cc6953839996a9c98c8907ee4065f3bf440599
Justin Sohn <jusohn2015@gmail.com>
2021-08-11 11:50:13 -0400

    commit tree a492ca92061f391c41117a9f9decae2186feb2e3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Justin',
+    streetAddress: '18330 Dunn Rd'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+} 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const copyEmployee = {...employee};
+    delete copyEmployee[key];
+    return copyEmployee;
+} 
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
